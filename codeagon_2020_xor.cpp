3bc712d50d9cef7887921e086821cf706dcc966d
devashishkatoriya <devashishkatoriya@gmail.com>
2020-09-28 11:35:11 +0530

    Added CodeAgon 2020 XOR Problem

---
+++ codeagon_2020_xor.cpp
@@ -0,0 +1,29 @@
+
+// Program to check if K consecutive elements have zero xor value
+// if not print number of changes to be made
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n = 4, k = 3, xor1, count = 0;
+    int arr[] = {1,2,3,1};
+
+    for(int i=0; i<=n-k; i++)
+    {
+        xor1 = arr[i];
+        for(int j=i+1; j<i+k; j++)
+        {
+            xor1 = xor1^arr[j];
+        }
+        if (xor1!=0)
+        {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
